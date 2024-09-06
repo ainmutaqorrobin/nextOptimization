ec18722f5b4382879dd606a625ed0d91f831db64
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-06 22:09:00 +0800

    create dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,8 +1,16 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
 
+const posts = await getPosts();
+
+export async function generateMetadata() {
+  const numberPosts = posts.length;
+  return {
+    title: `Browse all our ${numberPosts} posts.`,
+    description: "Browse all our posts.",
+  };
+}
 export default async function FeedPage() {
-  const posts = await getPosts();
   return (
     <>
       <h1>All posts by all users</h1>
